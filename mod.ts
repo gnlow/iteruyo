7d8c499f84d6eddd3e459a7395e8e6a2f27bc687
Gnlow <gnlow@protonmail.com>
2023-05-27 01:54:02 +0900

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,7 +1,10 @@
 // deno-lint-ignore-file no-this-alias
+
+type IterableLike<I> = Iterable<I> | (() => Iterator<I>)
+
 class Iteruyo<I> {
     iterable
-    constructor(iterable: Iterable<I> | (() => Iterator<I>)) {
+    constructor(iterable: IterableLike<I>) {
         if (typeof iterable == "function") {
             this.iterable = {
                 [Symbol.iterator]: iterable
@@ -15,6 +18,10 @@ class Iteruyo<I> {
         return this.iterable[Symbol.iterator]()
     }
 
+    static from(iterable: IterableLike<I>) {
+        return new Iteruyo(iterable)
+    }
+
     map<O>(f: (i: I) => O) {
         const that = this
         return new Iteruyo(function*() {
@@ -22,12 +29,29 @@ class Iteruyo<I> {
                 yield f(i)
         })
     }
+
+    filter(f: (i: I) => boolean) {
+        const that = this
+        return new Iteruyo(function*() {
+            for (const i of that)
+                if (f(i))
+                    yield i
+        })
+    }
+
+    length() {
+        let length = 0
+        for (const _ of this)
+            length++
+        return length
+    }
+
+    pipe<A>(f: (i: Iteruyo<I>) => A) {
+        return f(this)
+    }
 }
 
-const a = new Iteruyo([1, 2, 3])
-const b = a.map(x => x * 2)
-
-console.log(...b)
-console.log(...a)
-console.log(...b)
-console.log(...a)
+Iteruyo
+    .from([1, 2, 3])
+    .map(i => i * 2)
+    .pipe(x => console.log(...x))
